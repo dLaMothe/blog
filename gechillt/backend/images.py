91a2fc3b92c1a9312485923ae0f2fcafdbc4ee4c
David LaMothe <david.a.lamothe@gmail.com>
2018-05-10 14:41:30 -0500

    More python setup

---
+++ gechillt/backend/images.py
@@ -0,0 +1,19 @@
+import json
+
+import falcon
+
+class Resource(object):
+
+    def on_get(self, req, resp):
+        doc = {
+            'images': [
+                {
+                    'href': '/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png'
+                }
+            ]
+        }
+
+        # Create a JSON representation of the resource
+        resp.body = json.dumps(doc, ensure_ascii=False)
+
+        resp.status = falcon.HTTP_200
