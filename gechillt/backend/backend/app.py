c8ae51097f37de11c1108649fc5d1bc622e84161
David LaMothe <david.a.lamothe@gmail.com>
2018-05-21 12:10:41 +0200

    split up directory structure a bit

--- gechillt/backend/backend/app.py
+++ gechillt/backend/backend/app.py
@@ -2,25 +2,20 @@ import os
 
 import falcon
 
-
-from .images import Collection, Item, ImageStore
-from .comments import CommentsResource
-from .posts import PostsResource
+from backend.resources import images
 
 
 def create_app(image_store):
-    posts = PostsResource()
-    comments = CommentsResource()
     api = falcon.API()
 
-    api.add_route('/images', Collection(image_store))
-    api.add_route('/images/{name}', Item(image_store))
-    api.add_route('/posts', posts)
-    api.add_route('/comments', comments)
+    api.add_route('/images', images.Collection(image_store))
+    api.add_route('/images/{name}', images.Item(image_store))
+    #api.add_route('/posts', posts)
+    #api.add_route('/comments', comments)
     return api
 
 
 def get_app():
     storage_path = os.environ.get('BACKEND_STORAGE_PATH', '.')
-    image_store = ImageStore(storage_path)
+    image_store = images.ImageStore(storage_path)
     return create_app(image_store)
