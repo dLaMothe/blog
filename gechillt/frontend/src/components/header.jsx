0fb097019fd27f05ef379921d92660086c88d72f
David LaMothe <david.a.lamothe@gmail.com>
2018-06-03 13:04:41 +0200

    Add material-UI for icons

--- gechillt/frontend/src/components/header.jsx
+++ gechillt/frontend/src/components/header.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Search } from '@material-ui/icons/';
+import { Search } from '@material-ui/icons';
 
 export default class Header extends React.Component {
   render() {
