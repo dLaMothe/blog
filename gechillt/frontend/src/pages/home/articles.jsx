a629e6ccb3a47898b25da3ef4efbffd2e4728cf1
David LaMothe <david.a.lamothe@gmail.com>
2018-06-03 17:31:17 +0200

    Get initial articleList layout

--- gechillt/frontend/src/pages/home/articles.jsx
+++ gechillt/frontend/src/pages/home/articles.jsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
+import { ArrowBack, ArrowForward } from '@material-ui/icons';
 
 export default class Articles extends React.Component {
   render() {
     return (
       <div className="articles">
-        <div className="article__left" />
-        <div className="article__summary" />
-        <div className="article__right" />
+        <div className="articles__left">
+          <ArrowBack />
+        </div>
+        <div className="articles__summary">
+          <h4>Some Summary Text Here</h4>
+        </div>
+        <div className="articles__right">
+          <ArrowForward />
+        </div>
       </div>
     );
   }
