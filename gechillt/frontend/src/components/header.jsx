9e7b9f129e5951c97078c348229c5eaf20c0e345
David LaMothe <david.a.lamothe@gmail.com>
2018-06-03 13:00:41 +0200

    initial layout structure

--- gechillt/frontend/src/components/header.jsx
+++ gechillt/frontend/src/components/header.jsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Search } from '@material-ui/icons/';
 
 export default class Header extends React.Component {
   render() {
-    return <div className="header" />;
+    return (
+      <div className="header">
+        <div className="about-me">
+          <Link className="about-me--link" to="/about">
+            About Me
+          </Link>
+        </div>
+        <div className="title">
+          <div className="title__text">Title</div>
+        </div>
+        <div className="search">
+          <div className="search__icon">
+            <Search />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
