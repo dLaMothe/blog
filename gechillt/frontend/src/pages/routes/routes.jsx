c410a58d8e655b39f9a65c19ad01738b1a7dd57a
David LaMothe <david.a.lamothe@gmail.com>
2018-06-08 00:07:07 +0200

    Init about page

--- gechillt/frontend/src/pages/routes/routes.jsx
+++ gechillt/frontend/src/pages/routes/routes.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import Home from '../home/home';
+import About from '../about/about';
 
 export default class Router extends React.Component {
   render() {
     return (
       <div className="router-container">
         <Route exact={true} path="/" component={Home} />
+        <Route exact={true} path="/about" component={About} />
       </div>
     );
   }
