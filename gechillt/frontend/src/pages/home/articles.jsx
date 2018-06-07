15cad8e2710210724675c40e1e6d036e9e3c7ed7
David LaMothe <david.a.lamothe@gmail.com>
2018-06-08 01:28:33 +0200

    Add clickable link to article

--- gechillt/frontend/src/pages/home/articles.jsx
+++ gechillt/frontend/src/pages/home/articles.jsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import { ArrowBack, ArrowForward } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 export default class Articles extends React.Component {
   render() {
+    const dummyId = 15;
+
     return (
       <div className="articles">
         <div className="articles__left">
           <ArrowBack />
         </div>
         <span className="articles__date">20.04.1992</span>
-        <h4 className="articles__summary">Some Summary Text Here</h4>
+        <Link className="articles__summary" to={'/articles/' + dummyId}>
+          <h4>Some Summary Text Here</h4>
+        </Link>
         <h5 className="articles__subtitle">Some subtitle here</h5>
         <div className="articles__right">
           <ArrowForward />
