841c575fd0dfac2b1a1c97f640a8bf8bec146866
David LaMothe <david.a.lamothe@gmail.com>
2018-06-08 00:02:43 +0200

    Add footer text

--- gechillt/frontend/src/components/footer.jsx
+++ gechillt/frontend/src/components/footer.jsx
@@ -2,6 +2,15 @@ import * as React from 'react';
 
 export default class Footer extends React.Component {
   render() {
-    return <div className="footer" />;
+    return (
+      <div className="footer">
+        <div className="footer__text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor.
+          </p>
+        </div>
+      </div>
+    );
   }
 }
