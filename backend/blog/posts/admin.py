58fa22c71e2d47bac4202dd552bc96de3a1c47cf
David LaMothe <david.a.lamothe@gmail.com>
2019-11-09 19:49:29 +0100

    Add posts and categories to admin panels

--- backend/blog/posts/admin.py
+++ backend/blog/posts/admin.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from blog.posts.models import Post, Category
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
