c81b8e5bb09b64f498c0d624a31b04cad7f1cb1b
David LaMothe <david.a.lamothe@gmail.com>
2019-11-27 18:56:37 +0100

    Wire up API for individual articles

--- frontend/src/pages/article/article.jsx
+++ frontend/src/pages/article/article.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Tram } from '@material-ui/icons';
 import Markdown from '../../components/markdown';
-import mdPath from './test.md';
 import marked from 'marked';
 
 export default class Article extends React.Component {
@@ -10,45 +9,49 @@ export default class Article extends React.Component {
     this.state = { markdown: '' };
   }
 
-  componentWillMount() {
-    //TODO: Markdown will be sent via server API
-    fetch(mdPath)
-      .then(response => {
-        return response.text();
-      })
-      .then(text => {
+  componentDidMount() {
+    console.log(this.props.match);
+    fetch('/blog/posts/' + this.props.match.params.id + '/')
+      .then(response => response.json())
+      .then(article => {
+        console.log(article);
         this.setState({
-          markdown: marked(text),
-          id: this.props.match.params.id
+          article: article,
+          markdown: marked(article.body)
         });
       });
   }
 
   render() {
-    const dummyTags = ['these', 'are', 'tags'];
-    return (
-      <div className="article">
-        <div className="article__header">
-          <div className="article__date">29.4.1992</div>
-          <div className="article__icon">
-            <Tram />
+    const article = this.state.article;
+
+    if (article) {
+      return (
+        <div className="article">
+          <div className="article__header">
+            <div className="article__date">29.4.1992</div>
+            <div className="article__icon">
+              <Tram />
+            </div>
+            <div className="article__headline">
+              <b>article.title</b>
+              <hr className="article__hr" />
+              {article.categories.map((category, key) => {
+                return (
+                  <span key={key} className="article__tag">
+                    {'#' + category.name + ' '}
+                  </span>
+                );
+              })}
+            </div>
           </div>
-          <div className="article__headline">
-            <b>This is a long and fancy Headline as an example</b>
-            <hr className="article__hr" />
-            {dummyTags.map((tag, key) => {
-              return (
-                <span key={key} className="article__tag">
-                  {'#' + tag + ' '}
-                </span>
-              );
-            })}
+          <div className="article__body">
+            <Markdown markdown={this.state.markdown} />
           </div>
         </div>
-        <div className="article__body">
-          <Markdown markdown={this.state.markdown} />
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
   }
 }
