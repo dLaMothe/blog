a46b77a8d2a6ee77ee0a03f17d28cd617b4cdaac
David LaMothe <david.a.lamothe@gmail.com>
2019-12-30 13:32:36 +0100

    Remove NPM within django project

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import django_heroku
 from django.conf.global_settings import STATICFILES_FINDERS
-from npm.finders import npm_install
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,6 +131,4 @@ STATIC_URL = '/static/'
 
 # Place static in the same location as webpack build files
 STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
-STATICFILES_FINDERS.append('npm.finders.NpmFinder')
-npm_install()
 STATICFILES_DIRS = []
