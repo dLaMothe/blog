5ec74334c1d9262878ad601090b5d83c489d3444
David LaMothe <david.a.lamothe@gmail.com>
2020-01-01 19:09:21 +0100

    Format about-me using local article

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,7 +1,45 @@
 import * as React from 'react';
+import Article from '../article/article';
 
 export default class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { article: null };
+  }
+
+  componentDidMount() {
+    const readmePath = require('./about-me.md');
+
+    fetch(readmePath)
+      .then(response => {
+        return response.text();
+      })
+      .then(text => {
+        this.setState({
+          article: {
+            title: 'About Me',
+            body: text,
+            date_created: '2020.1.1',
+            categories: [{ name: 'about-me' }]
+          }
+        });
+      });
+  }
+
   render() {
-    return <div className="about">Testing</div>;
+    const article = this.state.article;
+
+    if (article) {
+      return (
+        <Article
+          title={article.title}
+          body={article.body}
+          date_created={article.date_created}
+          categories={article.categories}
+        />
+      );
+    } else {
+      return null;
+    }
   }
 }
