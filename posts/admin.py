1fffc5a0d5050a64a598e93d38d43920ddc9a1a8
David LaMothe <david.a.lamothe@gmail.com>
2021-01-02 00:04:18 +0100

    Add missing import

--- posts/admin.py
+++ posts/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.db import models
 from posts.models import Post, Category
 
 from pagedown.widgets import AdminPagedownWidget
