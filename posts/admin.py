5cacc76f646facd91d07e0200bc1370bb5800e26
David LaMothe <david.a.lamothe@gmail.com>
2021-01-01 23:55:14 +0100

    Add pagedown editor for text fields in Django

--- posts/admin.py
+++ posts/admin.py
@@ -4,9 +4,13 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from posts.models import Post, Category
 
+from pagedown.widgets import AdminPagedownWidget
+
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': AdminPagedownWidget}
+    }
 
 
 class CategoryAdmin(admin.ModelAdmin):
